db6ee198fdcc96967e8da02d4e5bf881fc4d2ed2
Brian Kennedy <bpkennedy@gmail.com>
2020-02-13 21:59:09 -0600

    Swap apache for nginx and better docker-compose toolchain

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,6 +17,7 @@ let linter;
 let baseDirectory;
 let srcDirectory;
 let themeDirectory;
+const npmPackagesToInstall = [];
 
 let packageJson = {
   name: '',
@@ -31,7 +32,7 @@ let packageJson = {
   engines: {
     npm: '>= 10.0.0'
   },
-  browserslist: [ "extends @wordpress/browserslist-config" ],
+  browserslist: ['extends @wordpress/browserslist-config'],
   dependencies: {},
   devDependencies: {}
 };
@@ -64,7 +65,7 @@ async function configureOptions() {
   packageJson.name = answers.name;
 
   if (linter.includes('css')) {
-    packageJson.scripts['lint-css'] = 'stylelint src/style.scss';
+    packageJson.scripts['lint-css'] = 'stylelint src/sass';
   }
 
   if (linter.includes('javascript')) {
@@ -79,22 +80,21 @@ async function configureOptions() {
   packageJson.scripts.webpack = 'webpack --config webpack.config.js';
   packageJson.scripts.lint =
     'npm run lint-css --if-present && npm run lint-js --if-present && npm run lint-php --if-present';
-  packageJson.scripts.build = 'npm run webpack && npm run clean-wp && npm run copy-to-wp';
-  packageJson.scripts['copy-to-wp'] = `cpx "dist/**/*.*" "wp-content/themes/${answers.name}"`;
-  packageJson.scripts['clean-wp'] = `rimraf "wp-content/themes/${answers.name}/*"`
-  packageJson.scripts.start = 'docker-compose up -d'
-  packageJson.scripts.backup = `docker exec -it ${answers.name}_db_1 /usr/bin/mysqldump -u wordpress -pwordpress wordpress > backups/backup_$(date +%Y-%m-%d-%H.%M.%S).sql`,
-  packageJson.scripts.restore = `docker exec ${answers.name}_db_1 /usr/bin/mysqldump -u wordpress -pwordpress wordpress < backups/$BACKUP_FILE_NAME.sql`
-  packageJson.scripts['php-install'] = "composer install"
+  packageJson.scripts.build =
+    'npm run webpack && npm run clean-wp && npm run copy-to-wp';
+  packageJson.scripts[
+    'copy-to-wp'
+  ] = `cpx "dist/**/*.*" "wordpress/wp-content/themes/${answers.name}"`;
+  packageJson.scripts[
+    'clean-wp'
+  ] = `rimraf "wordpress/wp-content/themes/${answers.name}/*"`;
+  packageJson.scripts.start = 'docker-compose up -d';
+  packageJson.scripts['php-install'] = 'composer install';
 }
 
 async function installOptions(ctx) {
-  await ctx.npmInstall();
-
   if (linter.includes('css')) {
-    ctx.npmInstall(['stylelint', 'stylelint-config-wordpress'], {
-      'save-dev': true
-    });
+    npmPackagesToInstall.push(...['stylelint', 'stylelint-config-wordpress']);
     ctx.fs.writeJSON(
       ctx.destinationPath(baseDirectory + '.stylelintrc.json'),
       styleLintConfig
@@ -102,9 +102,7 @@ async function installOptions(ctx) {
   }
 
   if (linter.includes('javascript')) {
-    ctx.npmInstall(['eslint', '@wordpress/eslint-plugin'], {
-      'save-dev': true
-    });
+    npmPackagesToInstall.push(...['eslint', '@wordpress/eslint-plugin']);
     ctx.fs.writeJSON(
       ctx.destinationPath(baseDirectory + '.eslintrc'),
       eslintConfig
@@ -120,8 +118,8 @@ async function installOptions(ctx) {
     ]);
   }
 
-  ctx.npmInstall(
-    [
+  npmPackagesToInstall.push(
+    ...[
       'webpack',
       'webpack-cli',
       '@wordpress/browserslist-config',
@@ -141,25 +139,26 @@ async function installOptions(ctx) {
       'postcss-preset-env',
       'cssnano',
       'mini-css-extract-plugin',
+      'modern-css-reset',
       'file-loader'
-    ],
-    { 'save-dev': true }
+    ]
   );
+
   ctx.fs.copy(
     ctx.templatePath('index.js'),
     ctx.destinationPath(srcDirectory + '/index.js')
   );
   ctx.fs.copy(
     ctx.templatePath('fx-style.scss'),
-    ctx.destinationPath(srcDirectory + '/fx-style.scss')
+    ctx.destinationPath(srcDirectory + '/sass/fx-style.scss')
   );
   ctx.fs.copy(
     ctx.templatePath('fx-sample.png'),
-    ctx.destinationPath(srcDirectory + '/fx-sample.png')
+    ctx.destinationPath(srcDirectory + '/images/fx-sample.png')
   );
   ctx.fs.copy(
     ctx.templatePath('fx-sample.ttf'),
-    ctx.destinationPath(srcDirectory + '/fx-sample.ttf')
+    ctx.destinationPath(srcDirectory + '/fonts/fx-sample.ttf')
   );
   ctx.fs.copyTpl(
     ctx.templatePath('webpack.config.js'),
@@ -167,13 +166,21 @@ async function installOptions(ctx) {
     { name: answers.name }
   );
   ctx.fs.copy(
+    ctx.templatePath('nginx.conf'),
+    ctx.destinationPath(baseDirectory + '/nginx.conf')
+  );
+  ctx.fs.copy(
     ctx.templatePath('.gitattributes'),
-    ctx.destinationPath(srcDirectory + '/.gitattributes')
+    ctx.destinationPath(baseDirectory + '/.gitattributes')
   );
   ctx.fs.copy(
     ctx.templatePath('.gitignore'),
-    ctx.destinationPath(srcDirectory + '/.gitignore')
+    ctx.destinationPath(baseDirectory + '/.gitignore')
   );
+
+  await ctx.npmInstall(npmPackagesToInstall, {
+    'save-dev': true
+  });
 }
 
 module.exports = class extends Generator {
@@ -272,12 +279,14 @@ module.exports = class extends Generator {
   install() {
     baseDirectory = './' + answers.name + '/';
     srcDirectory = baseDirectory + 'src';
-    themeDirectory = baseDirectory + `wp-content/themes/${answers.name}`;
-    const backupsDirectory = baseDirectory + 'backups';
+    themeDirectory =
+      baseDirectory + `wordpress/wp-content/themes/${answers.name}`;
 
     mkdirp.sync(baseDirectory);
-    mkdirp.sync(backupsDirectory);
     mkdirp.sync(srcDirectory);
+    mkdirp.sync(srcDirectory + '/sass');
+    mkdirp.sync(srcDirectory + '/fonts');
+    mkdirp.sync(srcDirectory + '/images');
     mkdirp.sync(themeDirectory);
 
     download('tidythemes/blankslate', 'master', async err => {
@@ -288,6 +297,11 @@ module.exports = class extends Generator {
         this.fs.move(
           this.destinationPath('master/**'),
           this.destinationPath(srcDirectory)
+        );
+
+        this.fs.move(
+          this.destinationPath(srcDirectory + '/style.css'),
+          this.destinationPath(srcDirectory + '/sass/theme-default.css')
         );
 
         this.fs.copy(
--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,2 +1,2 @@
 // Here for webpack js entrypoint for compilation, eslint task needs to match the `src/**/*.js` pattern, and for import of your theme's entrypoint .scss file
-import './fx-style.scss';
+import './sass/fx-style.scss';
